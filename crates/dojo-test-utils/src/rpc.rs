6c7251e3a3c442830d1e3d1f564c233564177e00
Elias Tazartes <66871571+Eikix@users.noreply.github.com>
2023-07-25 17:04:24 +0000

    fix: going giga crazy

--- crates/dojo-test-utils/src/rpc.rs
+++ crates/dojo-test-utils/src/rpc.rs
@@ -56,7 +56,8 @@ impl JsonRpcTransport for MockJsonRpcTransport {
         R: DeserializeOwned,
     {
         let method = serde_json::to_string(&method).unwrap().replace('\"', "");
-        let params = serde_json::to_string(&params).unwrap().replace(r#"\"#, "");
+        let params = serde_json::to_string(&params).unwrap();
+        let params = params.replace('\u{5C}', "");
 
         match self.responses.get(&(method.clone(), params.clone())) {
             Some(res) => serde_json::from_str(res).map_err(|e| MockError { msg: e.to_string() }),
